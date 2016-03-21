abfb3e69d886578a2071e68175b16a5d3757d459
Christopher Bartling <chris.bartling@gmail.com>
2016-03-21 10:12:56 -0500

    Uploading files via chunked transfer.

--- resumable-upload-ruby-client/upload_file.rb
+++ resumable-upload-ruby-client/upload_file.rb
@@ -6,18 +6,27 @@ module Samples
     class UploadFile
       require 'gcloud'
       require 'dotenv'
+      require 'faraday'
 
       def initialize
         Dotenv.load '.env'
+
+        # Use httpclient to avoid broken pipe errors with large uploads
+        # Faraday.default_adapter = :httpclient
+
+        # Only add the following statement if using Faraday >= 0.9.2
+        # Override gzip middleware with no-op for httpclient
+        # Faraday::Response.register_middleware gzip: Faraday::Response::Middleware
       end
 
       def upload_file(filename)
         gcloud = Gcloud.new ENV['PROJECT_ID'], ENV['SERVICE_ACCOUNT_KEY']
         storage = gcloud.storage
-        buckets = storage.buckets
-        buckets.each do |bucket|
-          puts bucket.name
-        end
+
+        bucket = storage.bucket 'foobar-001'
+
+        chunk_size = 256*1024   # 256KB chunk size
+        bucket.create_file(filename, File.basename(filename), chunk_size: chunk_size)
       end
     end
 
