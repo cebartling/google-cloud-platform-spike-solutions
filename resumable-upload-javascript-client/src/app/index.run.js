24c457a3eeee481db14c06c385328853e37c6c9a
Christopher Bartling <chris.bartling@gmail.com>
2016-03-25 13:23:58 -0500

    Editing the README.md.

--- resumable-upload-javascript-client/src/app/index.run.js
+++ resumable-upload-javascript-client/src/app/index.run.js
@@ -1,8 +1,8 @@
 export function runBlock(GClient, GApi, GAuth, toastr, GoogleCloudStorageService) {
     'ngInject';
 
-    let browserApiKey = '';
-    let clientId = '';
+    let browserApiKey = 'AIzaSyCxuWYC_0uOw99PhW5Uunz9KiWdbjUGnYs';
+    let clientId = '215385949726-n8r0dmckf2pi0h2o95vcrkq2s2te20th.apps.googleusercontent.com';
 
     GoogleCloudStorageService.login(browserApiKey, clientId).then(
         (user) => {
